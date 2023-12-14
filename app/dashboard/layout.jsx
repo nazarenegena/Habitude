d26208212dae068b59481f3c38a6c151a92911df
Nazarene Gena <genarene96@gmail.com>
2023-12-14 14:29:54 +0300

    updated dashboard layout

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,17 +1,13 @@
 import Sidebar from "@/components/sections/Sidebar";
-import { ModeToggle } from "@/components/toggle-mode";
 
 export default function DashboardLayout({ children }) {
   return (
-      // ensure navigation is smooth across multiple pages
-    //   dashboard has its own layout and page 
+    // ensure navigation is smooth across multiple pages
+    //   dashboard has its own layout and page
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-        <Sidebar />
-      
-      <main className="flex-grow p-6 md:overflow-y-auto md:p-12">
-        <ModeToggle/>
-        {children}
-      </main>
+      <Sidebar />
+
+      <main className="flex-grow">{children}</main>
     </div>
   );
 }
