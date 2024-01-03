553b9e4e299a6c3988cdce404f9be7951f38b11e
Nazarene Gena <genarene96@gmail.com>
2024-01-03 10:52:55 +0300

    resolved conflict

--- components/sections/Sidebar.jsx
+++ components/sections/Sidebar.jsx
@@ -1,42 +1,42 @@
-'use client';
-import Link from 'next/link';
-import React from 'react';
-import { HiHome } from 'react-icons/hi';
-import { MdDashboard } from 'react-icons/md';
-import { GiProgression } from 'react-icons/gi';
-import { FcPlanner } from 'react-icons/fc';
-import Lottie from 'lottie-react';
-import todoClip from '@/public/images/todo-clip.json';
-import { IoIosAdd } from 'react-icons/io';
-import { ImAccessibility } from 'react-icons/im';
-import { ModeToggle } from '../toggle-mode';
+"use client";
+import Link from "next/link";
+import React from "react";
+import { HiHome } from "react-icons/hi";
+import { MdDashboard } from "react-icons/md";
+import { GiProgression } from "react-icons/gi";
+import { FcPlanner } from "react-icons/fc";
+import Lottie from "lottie-react";
+import todoClip from "@/public/images/todo-clip.json";
+import { IoIosAdd } from "react-icons/io";
+import { ImAccessibility } from "react-icons/im";
+import { ModeToggle } from "../toggle-mode";
 
 const nav = [
-  {
-    link: '/dashboard',
-    title: 'Dashboard',
-    icon: <HiHome />
-  },
   // {
-  //   link: '/dashboard',
-  //   title: 'dashboard',
-  //   icon: <MdDashboard />
+  //   link: "/",
+  //   title: "home",
+  //   icon: <HiHome />,
   // },
   {
-    link: '/dashboard/progress',
-    title: 'progress',
-    icon: <GiProgression />
+    link: "/dashboard/tasks",
+    title: "tasks",
+    icon: <GiProgression />,
   },
   {
-    link: '/dashboard/tasks',
-    title: 'tasks',
-    icon: <GiProgression />
+    link: "/dashboard/progress",
+    title: "progress",
+    icon: <GiProgression />,
   },
   {
-    link: '/dashboard/planner',
-    title: 'planner',
-    icon: <FcPlanner />
-  }
+    link: "/dashboard/tasks",
+    title: "task",
+    icon: <GiProgression />,
+  },
+  {
+    link: "/dashboard/planner",
+    title: "planner",
+    icon: <FcPlanner />,
+  },
 ];
 const SidebarItem = ({ className, href, title, icon }) => {
   return (
@@ -44,7 +44,7 @@ const SidebarItem = ({ className, href, title, icon }) => {
       href={href}
       className={`${className} hover:bg-primary hover:rounded-md text-lg  px-5 py-5  mx-0 my-8 cursor-pointer flex flex-row transition ease-in-out delay-150 duration-300 items-center `}
     >
-      <span className='mr-6'>{icon}</span>
+      <span className="mr-6">{icon}</span>
       {title}
     </Link>
   );
@@ -52,9 +52,9 @@ const SidebarItem = ({ className, href, title, icon }) => {
 
 const Sidebar = () => {
   return (
-    <div className='flex'>
+    <div className="flex">
       <div
-        className='hidden
+        className="hidden
         bg-background
         border
         border-muted
@@ -62,15 +62,15 @@ const Sidebar = () => {
             flex-col
             gap-y-2
             p-2 md:flex
-            '
+            "
       >
-        <div className='flex mt-10'>
-          <p className='text-2xl ml-5 flex tracking-widest font-semibold'>
-            Hab <ImAccessibility className='mt-1' size={20} fill='#6937C8' />
+        <div className="flex mt-10">
+          <p className="text-2xl ml-5 flex tracking-widest font-semibold">
+            Hab <ImAccessibility className="mt-1" size={20} fill="#6937C8" />
             tude
           </p>
         </div>
-        <div className='mt-5'>
+        <div className="mt-5">
           {nav.map((navItem, index) => (
             <SidebarItem
               key={index}
@@ -80,14 +80,14 @@ const Sidebar = () => {
             />
           ))}
         </div>
-        <div className=''>
-          <Lottie animationData={todoClip} loop={true} className='ml-10' />
-          <div className='flex items-center justify-center bg-muted font-semibold cursor-pointer ml-10 py-2 w-44 rounded-lg transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-100 hover:bg-primary duration-300'>
-            <IoIosAdd className='mr-3' size={20} />
-            <Link href='/dashboard/tasks'>Add Task</Link>
+        <div className="">
+          <Lottie animationData={todoClip} loop={true} className="ml-10" />
+          <div className="flex items-center justify-center bg-muted font-semibold cursor-pointer ml-10 py-2 w-44 rounded-lg transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-100 hover:bg-primary duration-300">
+            <IoIosAdd className="mr-3" size={20} />
+            <Link href="/dashboard/tasks">Add Task</Link>
           </div>
         </div>
-        <div className='mt-12 ml-56'>
+        <div className="mt-12 ml-56">
           <ModeToggle />
         </div>
       </div>
