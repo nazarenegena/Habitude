80713ee50a0797b4bc479ac056bc42064e996fad
steve <mukamimuchiri822@gmail.com>
2024-01-10 09:26:49 +0300

    added quotes and stats to dashboard

--- components/charts/lineCharts.jsx
+++ components/charts/lineCharts.jsx
@@ -1,5 +1,5 @@
-"use client";
-import React from "react";
+'use client';
+import React from 'react';
 import {
   LineChart,
   Line,
@@ -7,80 +7,80 @@ import {
   XAxis,
   YAxis,
   Tooltip,
-  Legend,
-} from "recharts";
-import { Card } from "../ui/card";
+  Legend
+} from 'recharts';
+import { Card } from '../ui/card';
 
 const data = [
   {
-    day: "Monday",
+    day: 'Monday',
     daytime: 4,
     night: 2,
-    activity: 25,
+    activity: 25
   },
   {
-    day: "Tuesday",
+    day: 'Tuesday',
     daytime: 3,
     pv: 1,
-    activity: 28,
+    activity: 28
   },
   {
-    day: "Wednesday",
+    day: 'Wednesday',
     daytime: 2,
     night: 9,
-    activity: 29,
+    activity: 29
   },
   {
-    day: "Thursday",
+    day: 'Thursday',
     daytime: 2,
     night: 3,
-    activity: 20,
+    activity: 20
   },
   {
-    day: "Friday",
+    day: 'Friday',
     daytime: 10,
     night: 8,
-    activity: 18,
+    activity: 18
   },
   {
-    day: "Saturday",
+    day: 'Saturday',
     daytime: 2,
     night: 3,
-    activity: 25,
+    activity: 25
   },
   {
-    day: "Sunday",
+    day: 'Sunday',
     daytime: 3,
     night: 4,
-    activity: 21,
-  },
+    activity: 21
+  }
 ];
 
-const ProgressLineChart = () => (
-  <Card className="w-[100%] h-auto p-5">
+const ProgressLineChart = ({ stats }) => (
+  <Card className='w-[100%] h-300'>
     <LineChart
-      width={900}
-      height={400}
-      data={data}
+      width={500}
+      height={300}
+      data={stats}
       margin={{
         top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
+        right: 0,
+        left: 2,
+        bottom: 5
       }}
     >
-      <CartesianGrid strokeDasharray="2 2" stroke="#52525B" />
-      <XAxis dataKey="day" stroke="#52525B" strokeWidth={1} />
-      <YAxis stroke="#52525B" strokeWidth={1} />
+      <CartesianGrid strokeDasharray='2 2' stroke='#52525B' />
+      <XAxis dataKey='title' stroke='#52525B' strokeWidth={1} />
+      <YAxis stroke='#151516' strokeWidth={1} />
       <Tooltip />
       <Legend />
       <Line
-        type="monotone"
-        dataKey="night"
-        stroke="#6225C5"
+        type='monotone'
+        dataKey='value'
+        stroke='#6225C5'
         activeDot={{ r: 8 }}
       />
-      <Line type="monotone" dataKey="daytime" stroke="#E11D48" />
+      <Line type='monotone' dataKey='daytime' stroke='#E11D48' />
     </LineChart>
   </Card>
 );
